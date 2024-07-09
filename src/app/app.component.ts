cede0bde37dafd1a66567a2258cbe9ac3e4e3c7e
AnkitS_CodingRepo <123447794+AnkitSCodePortfolio@users.noreply.github.com>
2024-07-09 13:33:18 +0530

    init code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    HeaderComponent,
+    MatToolbarModule,
+    MatIconModule,
+    FooterComponent,
+    HomeComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
